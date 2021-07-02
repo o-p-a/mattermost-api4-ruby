37de5d16eaa1abf2fa2714ebc0dee0b787f9ad5a
Quentin Rousseau <contact@quent.in>
2021-07-02 16:46:44 -0700

    body

--- lib/mattermost/endpoint/dialogs.rb
+++ lib/mattermost/endpoint/dialogs.rb
@@ -4,7 +4,7 @@ module Mattermost
 	module Endpoint
 		module Dialogs
             def views_open(view)
-                post('/actions/dialogs/open', view: view.to_json)
+                post('/actions/dialogs/open', body: view.to_json)
             end
 		end
 	end
