72816410ee6fc3f7428ec497da45a81beb379513
Quentin Rousseau <contact@quent.in>
2021-07-03 21:06:56 -0700

    Update dialogs.rb

--- lib/mattermost/endpoint/dialogs.rb
+++ lib/mattermost/endpoint/dialogs.rb
@@ -3,9 +3,9 @@ require 'json'
 module Mattermost
 	module Endpoint
 		module Dialogs
-            def views_open(view)
-                post('/actions/dialogs/open', body: view)
-            end
+            		def views_open(view)
+                		post('/actions/dialogs/open', body: view.to_json)
+			end
 		end
 	end
 end
